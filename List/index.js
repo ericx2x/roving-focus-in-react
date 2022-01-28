a516156e77a425c6ec19259edcd5f780395d3d73
ericx2x <ericjliam@gmail.com>
2022-01-28 12:14:51 -0500

    edit

--- List/index.js
+++ List/index.js
@@ -1,25 +1,39 @@
-import React from "react";
+import React from 'react';
 
-import Item from "./Item";
-import useRoveFocus from "./useRoveFocus";
-import characters from "./onePunchManCharacters";
+import Item from './Item';
+import useRoveFocus from './useRoveFocus';
+import characters from './onePunchManCharacters';
 
 const List = () => {
   const [focus, setFocus] = useRoveFocus(characters.length);
 
   return (
-    <ul>
-      {characters.map((character, index) => (
-        <Item
-          key={character}
-          setFocus={setFocus}
-          index={index}
-          focus={focus === index}
-          character={character}
-        />
-      ))}
-    </ul>
+    <>
+      <ul>
+        {characters.map((character, index) => (
+          <Item
+            key={character}
+            setFocus={setFocus}
+            index={index}
+            focus={focus === index}
+            character={character}
+          />
+        ))}
+      </ul>
+      <ul>
+        {characters.map((character, index) => (
+          <Item
+            key={character}
+            setFocus={setFocus}
+            index={index}
+            focus={focus === index}
+            character={character}
+          />
+        ))}
+      </ul>
+    </>
   );
 };
 
 export default List;
+
